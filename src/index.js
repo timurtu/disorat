2996e0bc94770a22ad1bbeef5841e9795e8dcebe
Timur Uzel <timur@Timurs-MacBook-Pro.local>
2016-11-11 15:25:44 -0500

    list remove

--- src/index.js
+++ src/index.js
@@ -10,18 +10,28 @@ import db from './db'
 import {onError} from './utils'
 
 
-db.set('message1', 'hello, this is doge')
-db.set('message2', 'hello, no this is spider')
-
 co(function*() {
 
+  // Set key-value pairs
+  db.set('message1', 'hello, this is doge')
+  db.set('message2', 'hello, no this is spider')
+
+  // Get key-value pairs
   const msg1 = yield db.getAsync('message1')
   const msg2 = yield db.getAsync('message2')
 
   log('cyan', msg1)
   log('cyan', msg2)
 
-  yield db.lpushAsync('messages', 'Hello, this is dog')
+  const message = 'Hello, this is dog'
+  // Push a message item to a list
+  const length = yield db.lpushAsync('messages', message)
+  log('green', length)
+
+  // Remove a message item from the list
+  yield db.lremAsync('messages', -1, message)
+
+  // Get all items from a list
   const list = yield db.lrangeAsync('messages', 0, -1)
   log('blue', list)
 
