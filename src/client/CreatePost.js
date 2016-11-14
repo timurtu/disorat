2777b2bf3245530b9d6aba2e32b0e1eb9a9f3657
Timur Uzel <timur@Timurs-MacBook-Pro.local>
2016-11-14 16:17:10 -0500

    gotta add more data and create posts

--- src/client/CreatePost.js
+++ src/client/CreatePost.js
@@ -34,44 +34,42 @@ class CreatePost extends React.Component {
 
   render() {
     return (
-      <div className="ui segment">
-        <form onSubmit={this.createPost} className="ui form">
-          <div className="field">
-            <label>Title</label>
-            <input onChange={this.handleTitleChange} name="title" type="text" placeholder="Short Explanation"/>
-          </div>
-          <div className="two fields">
-            <div className="required field">
-              <label>Option 1</label>
-              <input name="first" type="text" placeholder="First Option"/>
-            </div>
-            <div className="required field">
-              <label>Option 2</label>
-              <input type="text" placeholder="Second Option"/>
-            </div>
+      <form onSubmit={this.createPost} className="ui form">
+        <div className="field">
+          <label>Title</label>
+          <input onChange={this.handleTitleChange} name="title" type="text" placeholder="Short Explanation"/>
+        </div>
+        <div className="two fields">
+          <div className="required field">
+            <label>Option 1</label>
+            <input name="first" type="text" placeholder="First Option"/>
           </div>
           <div className="required field">
-            <label>How long will voting last until a winner is declared?</label>
-            <div className="ui selection dropdown">
-              <div className="default text">Select</div>
-              <i className="dropdown icon"></i>
-              <input type="hidden" name="gender"/>
-              <div className="menu">
-                <div className="item" value="forever">Forever</div>
-                <div className="item" value="month">1 Month</div>
-                <div className="item" value="week">1 Week</div>
-                <div className="item" value="day">1 Day</div>
-              </div>
+            <label>Option 2</label>
+            <input type="text" placeholder="Second Option"/>
+          </div>
+        </div>
+        <div className="required field">
+          <label>How long will voting last until a winner is declared?</label>
+          <div className="ui selection dropdown">
+            <div className="default text">Select</div>
+            <i className="dropdown icon"></i>
+            <input type="hidden" name="gender"/>
+            <div className="menu">
+              <div className="item" value="forever">Forever</div>
+              <div className="item" value="month">1 Month</div>
+              <div className="item" value="week">1 Week</div>
+              <div className="item" value="day">1 Day</div>
             </div>
           </div>
-          <Link to="/" className="ui button">
-            Cancel
-          </Link>
-          <button className="ui right floated color blue submit button">
-            Create
-          </button>
-        </form>
-      </div>
+        </div>
+        <Link to="/" className="ui button">
+          Cancel
+        </Link>
+        <button className="ui right floated color blue submit button">
+          Create
+        </button>
+      </form>
     )
   }
 }
