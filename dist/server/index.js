6a0e5e39222e3023a5daa790fafc9b46967faab6
Timur Uzel <timur@Timurs-MacBook-Pro.local>
2016-11-17 13:12:37 -0500

    sorting posts by most votes

--- dist/server/index.js
+++ dist/server/index.js
@@ -28,10 +28,21 @@ _app2.default.get('*', sendIndex);
 
 _app2.default.post('/posts', function (req, res) {
   _db2.default.lrangeAsync('posts', 0, -1).then(function (posts) {
+
+    var sortedPosts = posts.sort(function (x, y) {
+      var post1 = JSON.parse(x);
+      var post2 = JSON.parse(y);
+
+      var post1Total = post1.option1votes + post1.option2votes;
+      var post2Total = post2.option1votes + post2.option2votes;
+
+      return post2Total - post1Total;
+    });
+
     if (req.query.limit >= 0) {
-      res.json(posts.slice(0, req.query.limit));
+      res.json(sortedPosts.slice(0, req.query.limit));
     } else {
-      res.json(posts);
+      res.json(sortedPosts);
     }
   }).catch(_utils.onError);
 });
--- src/server/index.js
+++ src/server/index.js
@@ -18,10 +18,21 @@ app.get('*', sendIndex)
 app.post('/posts', (req, res) => {
   db.lrangeAsync('posts', 0, -1)
     .then(posts => {
+
+      const sortedPosts = posts.sort(function(x, y) {
+        const post1 = JSON.parse(x)
+        const post2 = JSON.parse(y)
+
+        const post1Total = post1.option1votes + post1.option2votes
+        const post2Total = post2.option1votes + post2.option2votes
+
+        return post2Total - post1Total
+      })
+
       if (req.query.limit >= 0) {
-        res.json(posts.slice(0, req.query.limit))
+        res.json(sortedPosts.slice(0, req.query.limit))
       } else {
-        res.json(posts)
+        res.json(sortedPosts)
       }
     }).catch(onError)
 })
